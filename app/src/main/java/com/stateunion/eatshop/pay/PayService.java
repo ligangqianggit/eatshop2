ce62ea4b6913e0d970e6283841f4aff610edadfc
ligangqianggit <962027269@qq.com>
2017-12-26 17:04:56 +0800

    更新支付页activity

--- app/src/main/java/com/stateunion/eatshop/pay/PayService.java
+++ app/src/main/java/com/stateunion/eatshop/pay/PayService.java
@@ -12,7 +12,6 @@ import com.stateunion.eatshop.view.baseactivity.BaseActivity;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
-import static android.R.attr.type;
 
 /**
  * Created by zhangguozheng on 2017/12/14.
@@ -31,50 +30,50 @@ public class PayService extends BaseActivity{
         init();
     }
     private void init(){
-         rl_zhifubao_pay= (RelativeLayout) findViewById(R.id.rl_zhifubao_pay);
-        rl_weixin_pay=(RelativeLayout) findViewById(R.id.rl_weixin_pay);
-        zfbPay= (ImageView) findViewById(R.id.iv_right_circle1);
-        wxPay= (ImageView) findViewById(R.id.iv_right_circle2);
-        buyNow=(Button) findViewById(R.id.bt_buy);
-        zfbPay.setImageResource(R.mipmap.right_circle_true);
-        rl_zhifubao_pay.setOnClickListener(listener);
-        rl_weixin_pay.setOnClickListener(listener);
-        buyNow.setOnClickListener(listener);
+//         rl_zhifubao_pay= (RelativeLayout) findViewById(R.id.rl_zhifubao_pay);
+//        rl_weixin_pay=(RelativeLayout) findViewById(R.id.rl_weixin_pay);
+//        zfbPay= (ImageView) findViewById(R.id.iv_right_circle1);
+//        wxPay= (ImageView) findViewById(R.id.iv_right_circle2);
+//        buyNow=(Button) findViewById(R.id.bt_buy);
+//        zfbPay.setImageResource(R.mipmap.right_circle_true);
+//        rl_zhifubao_pay.setOnClickListener(listener);
+//        rl_weixin_pay.setOnClickListener(listener);
+//        buyNow.setOnClickListener(listener);
 
     }
 
       private View.OnClickListener listener=new View.OnClickListener() {
           @Override
           public void onClick(View v) {
-          switch (v.getId()){
-              case R.id.bt_buy:
-                if (isWxPay){
-                    if(msgApi==null)
-                        msgApi= WXAPIFactory.createWXAPI(PayService.this,null);
-                    if(msgApi.isWXAppInstalled()&&msgApi.isWXAppSupportAPI()){
-                        payWithWX();
-                    } else {
-                        showError("微信客户端未安装，请确认");
-                    }
-
-                } else {
-                    payWithZFB();
-                }
-                  break;
-              case R.id.rl_zhifubao_pay:
-
-                  zfbPay.setImageResource(R.mipmap.right_circle_true);
-                  wxPay.setImageResource(R.mipmap.right_circle_false);
-                  isWxPay = false;
-                  break;
-              case R.id.rl_weixin_pay:
-                  wxPay.setImageResource(R.mipmap.right_circle_true);
-                  zfbPay.setImageResource(R.mipmap.right_circle_false);
-                  isWxPay = true;
-                  break;
-              default:
-                  break;
-          }
+//          switch (v.getId()){
+//              case R.id.bt_buy:
+//                if (isWxPay){
+//                    if(msgApi==null)
+//                        msgApi= WXAPIFactory.createWXAPI(PayService.this,null);
+//                    if(msgApi.isWXAppInstalled()&&msgApi.isWXAppSupportAPI()){
+//                        payWithWX();
+//                    } else {
+//                        showError("微信客户端未安装，请确认");
+//                    }
+//
+//                } else {
+//                    payWithZFB();
+//                }
+//                  break;
+//              case R.id.rl_zhifubao_pay:
+//
+//                  zfbPay.setImageResource(R.mipmap.right_circle_true);
+//                  wxPay.setImageResource(R.mipmap.right_circle_false);
+//                  isWxPay = false;
+//                  break;
+//              case R.id.rl_weixin_pay:
+//                  wxPay.setImageResource(R.mipmap.right_circle_true);
+//                  zfbPay.setImageResource(R.mipmap.right_circle_false);
+//                  isWxPay = true;
+//                  break;
+//              default:
+//                  break;
+//          }
           }
       };
 

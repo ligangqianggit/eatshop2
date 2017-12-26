8941faba19be817622318784d0e6467b50452abb
ligangqianggit <962027269@qq.com>
2017-12-26 17:01:06 +0800

    新增上传菜单早中晚加班fragmeng

---
+++ app/src/main/java/com/stateunion/eatshop/view/mainfrment/UpLunthFragment.java
@@ -0,0 +1,23 @@
+package com.stateunion.eatshop.view.mainfrment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.stateunion.eatshop.R;
+
+/**
+ * Created by 青春 on 2017/12/11.
+ */
+
+public class UpLunthFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_uplunch,container,false);
+        return view;
+    }
+}

b97062eedd4729a680fab8ae7f8f5e3855d938d7
ligangqianggit <962027269@qq.com>
2018-01-02 17:08:08 +0800

    frag修改

--- app/src/main/java/com/stateunion/eatshop/view/mainfrment/PersFragment.java
+++ app/src/main/java/com/stateunion/eatshop/view/mainfrment/PersFragment.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.stateunion.eatshop.R;
+import com.stateunion.eatshop.pay.PayResult;
+import com.stateunion.eatshop.util.AppSessionEngine;
 import com.stateunion.eatshop.view.baseactivity.HistoryActivity;
 import com.stateunion.eatshop.view.baseactivity.OpinActivity;
+import com.stateunion.eatshop.view.baseactivity.ZiLiaoActivity;
 import com.stateunion.eatshop.view.basefrment.BaseFragment;
 
 /**
@@ -16,7 +20,8 @@ import com.stateunion.eatshop.view.basefrment.BaseFragment;
 
 public class PersFragment extends BaseFragment {
     private Context context=null;
-    private LinearLayout llyt_preson_fankui,llyt_preson_history;
+    private LinearLayout llyt_preson_fankui,llyt_preson_history,llyt_preson_zilaio;
+    private TextView tv_preson_user,tv_preson_iccard,tv_preson_phone,tv_preson_location;
     @Override
     public int getLayoutId() {
         return R.layout.frag_preson;
@@ -25,6 +30,17 @@ public class PersFragment extends BaseFragment {
     @Override
     public void createView(View rootView) {
         context=rootView.getContext();
+        intview(rootView);
+
+        llyt_preson_zilaio=(LinearLayout) rootView.findViewById(R.id.llyt_preson_zilaio);
+        llyt_preson_zilaio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(context, ZiLiaoActivity.class);
+                startActivity(intent);
+            }
+        });
+
         llyt_preson_fankui=(LinearLayout) rootView.findViewById(R.id.llyt_preson_fankui);
         llyt_preson_fankui.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +58,16 @@ public class PersFragment extends BaseFragment {
                 startActivity(intent);
             }
         });
+    }
+    public void intview(View view){
+        AppSessionEngine.getLoginInfo().getName();
+                tv_preson_user= (TextView) view.findViewById(R.id.tv_preson_user);
+                tv_preson_iccard=(TextView) view.findViewById(R.id.tv_preson_iccard);
+                tv_preson_phone=(TextView) view.findViewById(R.id.tv_preson_phone);
+                tv_preson_location=(TextView) view.findViewById(R.id.tv_preson_location);
+        tv_preson_user.setText(AppSessionEngine.getLoginInfo().getName());
+        tv_preson_iccard.setText(AppSessionEngine.getLoginInfo().getGonghao());
+        tv_preson_phone.setText(AppSessionEngine.getLoginInfo().getPhone());
     }
 
     @Override

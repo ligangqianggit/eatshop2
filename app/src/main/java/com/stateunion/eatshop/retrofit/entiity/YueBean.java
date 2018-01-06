10b55f90e5da08527c1adacc781a9f8e9fd55c1b
ligangqianggit <962027269@qq.com>
2018-01-06 14:52:11 +0800

    新增余额工具类

---
+++ app/src/main/java/com/stateunion/eatshop/retrofit/entiity/YueBean.java
@@ -0,0 +1,18 @@
+package com.stateunion.eatshop.retrofit.entiity;
+
+import com.stateunion.eatshop.retrofit.bean.BaseBean;
+
+/**
+ * Created by 青春 on 2018/1/6.
+ */
+
+public class YueBean extends BaseBean{
+    private YueEntity body;
+    public YueEntity getBody() {
+        return body;
+    }
+
+    public void setBody(YueEntity body) {
+        this.body = body;
+    }
+}
---
+++ app/src/main/java/com/stateunion/eatshop/retrofit/entiity/YueEntity.java
@@ -0,0 +1,22 @@
+package com.stateunion.eatshop.retrofit.entiity;
+
+/**
+ * Created by 青春 on 2018/1/6.
+ */
+
+public class YueEntity {
+
+    /**
+     * yumoney :
+     */
+
+    private String yumoney;
+
+    public String getYumoney() {
+        return yumoney;
+    }
+
+    public void setYumoney(String yumoney) {
+        this.yumoney = yumoney;
+    }
+}

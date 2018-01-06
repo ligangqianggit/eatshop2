857a054d371b70204a5739551857ee4b4941e2c8
ligangqianggit <962027269@qq.com>
2018-01-06 14:49:40 +0800

    修改资料页面

--- app/src/main/java/com/stateunion/eatshop/view/baseactivity/ZiLiaoActivity.java
+++ app/src/main/java/com/stateunion/eatshop/view/baseactivity/ZiLiaoActivity.java
@@ -1,38 +1,92 @@
 package com.stateunion.eatshop.view.baseactivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.webkit.ValueCallback;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
 import com.stateunion.eatshop.R;
+import com.stateunion.eatshop.util.CircleTransform;
+import com.stateunion.eatshop.util.ImageUtil;
+import com.stateunion.eatshop.util.PermissionUtil;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by 青春 on 2018/1/1.
  */
 
 public class ZiLiaoActivity extends BaseActivity{
-    private TextView tv_ziliao_date;
-    // 用来保存年月日：
-    private int mYear;
-    private int mMonth;
-    private int mDay;
-    // 声明一个独一无二的标识，来作为要显示DatePicker的Dialog的ID：
-    static final int DATE_DIALOG_ID = 0;
-    private ImageView iv_ziliao_back;
+    private EditText ed_ziliao_phone,ed_ziliao_dizhi;
+    private ImageView iv_ziliao_back,tv_ziliao_touxiang;
+    private String imageurl,shengri,phone,zhuzhi;
+    private Button bt_ziliao;
+
+    private static final int REQUEST_CODE_PICK_IMAGE = 0;
+    private static final int REQUEST_CODE_IMAGE_CAPTURE = 1;
+    private long clickTime = 0;
+    private Intent mSourceIntent;
+    private ValueCallback<Uri> mUploadMsg;
+    public ValueCallback<Uri[]> mUploadMsgForAndroid5;
+
+    private static final int P_CODE_PERMISSIONS = 101;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ziliao);
+        Intent intent=getIntent();
+        imageurl=intent.getStringExtra("imageurl");
+        shengri=intent.getStringExtra("shengri");
+        phone=intent.getStringExtra("phone");
+        zhuzhi=intent.getStringExtra("zhuzhi");
         intview();
     }
     public void intview(){
+        tv_ziliao_touxiang= (ImageView) findViewById(R.id.tv_ziliao_touxiang);
+        ed_ziliao_phone= (EditText) findViewById(R.id.ed_ziliao_phone);
+        ed_ziliao_dizhi= (EditText) findViewById(R.id.ed_ziliao_dizhi);
+        bt_ziliao= (Button) findViewById(R.id.bt_ziliao);
+        bt_ziliao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //提交修改资料
+
+            }
+        });
+        ed_ziliao_phone.setText(phone);
+        ed_ziliao_dizhi.setText(zhuzhi);
+        Picasso.with(this).load(imageurl).resize(200, 200).transform
+                (new CircleTransform()).into(tv_ziliao_touxiang);
+        //选择图片
+        tv_ziliao_touxiang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showOptions();
+            }
+        });
+
         iv_ziliao_back= (ImageView) findViewById(R.id.iv_ziliao_back);
         iv_ziliao_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,55 +95,218 @@ public class ZiLiaoActivity extends BaseActivity{
             }
         });
 
-
-
-        tv_ziliao_date= (TextView) findViewById(R.id.tv_ziliao_date);
-        // 获得当前的日期：
-        final Calendar currentDate = Calendar.getInstance();
-        mYear = currentDate.get(Calendar.YEAR);
-        mMonth = currentDate.get(Calendar.MONTH);
-        mDay = currentDate.get(Calendar.DAY_OF_MONTH);
-// 设置文本的内容：
-        tv_ziliao_date.setText(new StringBuilder().append(mYear).append("年")
-                .append(mMonth + 1).append("月")// 得到的月份+1，因为从0开始
-                .append(mDay).append("日"));
-
-        tv_ziliao_date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDialog(DATE_DIALOG_ID);
-            }
-        });
     }
-    // 需要定义弹出的DatePicker对话框的事件监听器：
-    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth) {
-            mYear = year;
-            mMonth = monthOfYear;
-            mDay = dayOfMonth;
-
-            String str1 = new String(new StringBuilder().append(mYear)
-                    .append("年").append(mMonth + 1).append("月")// 得到的月份+1，因为从0开始
-                    .append(mDay).append("日"));
 
 
-// 设置文本的内容：
-            tv_ziliao_date.setText(str1);
-        }
-    };
+    public void showOptions() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setOnCancelListener(new DialogOnCancelListener());
 
+        alertDialog.setTitle("请选择操作");
+        // gallery, camera.
 
-    /**
-     * 当Activity调用showDialog函数时会触发该函数的调用：
-     */
+        String[] options = {"相册", "拍照"};
+
+        alertDialog.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == 0) {
+                            if (PermissionUtil.isOverMarshmallow()) {
+                                if (!PermissionUtil.isPermissionValid(ZiLiaoActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                                    Toast.makeText(ZiLiaoActivity.this,
+                                            "请去\"设置\"中开启本应用的图片媒体访问权限",
+                                            Toast.LENGTH_SHORT).show();
+                                    restoreUploadMsg();
+                                    requestPermissionsAndroidM();
+                                    return;
+                                }
+                            }
+                            try {
+                                mSourceIntent = ImageUtil.choosePicture();
+                                startActivityForResult(mSourceIntent, REQUEST_CODE_PICK_IMAGE);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                Toast.makeText(ZiLiaoActivity.this,
+                                        "请去\"设置\"中开启本应用的图片媒体访问权限",
+                                        Toast.LENGTH_SHORT).show();
+                                restoreUploadMsg();
+                            }
+
+                        } else {
+                            if (PermissionUtil.isOverMarshmallow()) {
+                                if (!PermissionUtil.isPermissionValid(ZiLiaoActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                                    Toast.makeText(ZiLiaoActivity.this,
+                                            "请去\"设置\"中开启本应用的图片媒体访问权限",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    restoreUploadMsg();
+                                    requestPermissionsAndroidM();
+                                    return;
+                                }
+
+                                if (!PermissionUtil.isPermissionValid(ZiLiaoActivity.this, Manifest.permission.CAMERA)) {
+                                    Toast.makeText(ZiLiaoActivity.this,
+                                            "请去\"设置\"中开启本应用的相机权限",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    restoreUploadMsg();
+                                    requestPermissionsAndroidM();
+                                    return;
+                                }
+                            }
+                            try {
+                                mSourceIntent = ImageUtil.takeBigPicture();
+                                startActivityForResult(mSourceIntent, REQUEST_CODE_IMAGE_CAPTURE);
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                Toast.makeText(ZiLiaoActivity.this,
+                                        "请去\"设置\"中开启本应用的相机和图片媒体访问权限",
+                                        Toast.LENGTH_SHORT).show();
+                                restoreUploadMsg();
+                            }
+                        }
+                    }
+                }
+        );
+
+        alertDialog.show();
+    }
+
     @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case DATE_DIALOG_ID:
-                return new DatePickerDialog(this, mDateSetListener, mYear, mMonth,
-                        mDay);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            if (mUploadMsg != null) {
+                mUploadMsg.onReceiveValue(null);
+            }
+            if (mUploadMsgForAndroid5 != null) {         // for android 5.0+
+                mUploadMsgForAndroid5.onReceiveValue(null);
+            }
+            return;
         }
-        return null;
+        switch (requestCode) {
+            case REQUEST_CODE_IMAGE_CAPTURE:
+            case REQUEST_CODE_PICK_IMAGE: {
+                try {
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                        if (mUploadMsg == null) {
+                            return;
+                        }
+                        String sourcePath = ImageUtil.retrievePath(this, mSourceIntent, data);
+                        Log.v("eatshop","==========="+sourcePath);
+                        if (TextUtils.isEmpty(sourcePath) || !new File(sourcePath).exists()) {
+                            Log.e("eatshop", "sourcePath empty or not exists.");
+                            break;
+                        }
+                        Uri uri = Uri.fromFile(new File(sourcePath));
+                        mUploadMsg.onReceiveValue(uri);
+                    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        if (mUploadMsgForAndroid5 == null) {        // for android 5.0+
+                            return;
+                        }
+                        String sourcePath = ImageUtil.retrievePath(this, mSourceIntent, data);
+                        Log.v("eatshop","+++++++++++++++++++"+sourcePath);
+                        if (TextUtils.isEmpty(sourcePath) || !new File(sourcePath).exists()) {
+                            Log.e("eatshop", "sourcePath empty or not exists.");
+                            break;
+                        }
+                        Uri uri = Uri.fromFile(new File(sourcePath));
+                        mUploadMsgForAndroid5.onReceiveValue(new Uri[]{uri});
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+
     }
+
+    private void fixDirPath() {
+        String path = ImageUtil.getDirPath();
+        Log.v("eatshop",path);
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }
+
+    private class DialogOnCancelListener implements DialogInterface.OnCancelListener {
+        @Override
+        public void onCancel(DialogInterface dialogInterface) {
+            restoreUploadMsg();
+        }
+    }
+
+    private void restoreUploadMsg() {
+        if (mUploadMsg != null) {
+            mUploadMsg.onReceiveValue(null);
+            mUploadMsg = null;
+        } else if (mUploadMsgForAndroid5 != null) {
+            mUploadMsgForAndroid5.onReceiveValue(null);
+            mUploadMsgForAndroid5 = null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case P_CODE_PERMISSIONS:
+                requestResult(permissions, grantResults);
+                restoreUploadMsg();
+                break;
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+//权限
+    private void requestPermissionsAndroidM() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            List<String> needPermissionList = new ArrayList<>();
+            needPermissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            needPermissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            needPermissionList.add(Manifest.permission.CAMERA);
+            PermissionUtil.requestPermissions(ZiLiaoActivity.this, P_CODE_PERMISSIONS, needPermissionList);
+        } else {
+            return;
+        }
+    }
+    //权限
+    public void requestResult(String[] permissions, int[] grantResults) {
+        ArrayList<String> needPermissions = new ArrayList<String>();
+        for (int i = 0; i < grantResults.length; i++) {
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                if (PermissionUtil.isOverMarshmallow()) {
+
+                    needPermissions.add(permissions[i]);
+                }
+            }
+        }
+
+        if (needPermissions.size() > 0) {
+            StringBuilder permissionsMsg = new StringBuilder();
+
+            for (int i = 0; i < needPermissions.size(); i++) {
+                String strPermissons = needPermissions.get(i);
+
+                if (Manifest.permission.WRITE_EXTERNAL_STORAGE.equals(strPermissons)) {
+                    permissionsMsg.append("," + getString(R.string.permission_storage));
+
+                } else if (Manifest.permission.READ_EXTERNAL_STORAGE.equals(strPermissons)) {
+                    permissionsMsg.append("," + getString(R.string.permission_storage));
+
+                } else if (Manifest.permission.CAMERA.equals(strPermissons)) {
+                    permissionsMsg.append("," + getString(R.string.permission_camera));
+
+                }
+            }
+            String strMessage = "请允许使用\"" + permissionsMsg.substring(1).toString() + "\"权限, 以正常使用APP的所有功能.";
+            Toast.makeText(ZiLiaoActivity.this, strMessage, Toast.LENGTH_SHORT).show();
+
+        } else {
+            return;
+        }
+    }
+
 }

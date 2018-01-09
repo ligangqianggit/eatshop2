a7309184ec759f3fc6a2538801a2e3635ff76ddc
ligangqianggit <962027269@qq.com>
2018-01-09 20:42:52 +0800

    新建员工评价页

---
+++ app/src/main/java/com/stateunion/eatshop/view/baseactivity/YuangongPingjiaActivity.java
@@ -0,0 +1,19 @@
+package com.stateunion.eatshop.view.baseactivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.stateunion.eatshop.R;
+
+/**
+ * Created by 青春 on 2018/1/8.
+         */
+
+public class YuangongPingjiaActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yuangongpingjia);
+
+    }
+}

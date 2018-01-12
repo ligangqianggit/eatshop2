ba6c949135100e419bf990325e2db22d0f18f1b4
ligangqianggit <962027269@qq.com>
2018-01-12 09:31:16 +0800

    修改orderbean

--- app/src/main/java/com/stateunion/eatshop/retrofit/entiity/OrderBean.java
+++ app/src/main/java/com/stateunion/eatshop/retrofit/entiity/OrderBean.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class OrderBean extends BaseBean{
     public OrderBeanInfo body;
 
-    public OrderBeanInfo getBody() {
+    public  OrderBeanInfo getBody() {
         return body;
     }
 
@@ -51,6 +51,22 @@ public class OrderBean extends BaseBean{
             public String goods_num;
             private String foot_name;
             private String jiage;
+            private int id;
+            private int cid;
+            private String utime;
+            private String ctime;
+            private int num;
+            private String foodtupian;
+            private String foot_jianjie;
+            private String riqi;
+            private String fenlei;
+            private String chushi;
+            private String tui_time;
+            private String yuyue_start;
+            private String dingdanid;
+            private String shiwu_id;
+            private String shijianduan;
+
 
             public String getJiezhi() {
                 return jiezhi;
@@ -82,6 +98,126 @@ public class OrderBean extends BaseBean{
 
             public void setJiage(String jiage) {
                 this.jiage = jiage;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public int getCid() {
+                return cid;
+            }
+
+            public void setCid(int cid) {
+                this.cid = cid;
+            }
+
+            public String getUtime() {
+                return utime;
+            }
+
+            public void setUtime(String utime) {
+                this.utime = utime;
+            }
+
+            public String getCtime() {
+                return ctime;
+            }
+
+            public void setCtime(String ctime) {
+                this.ctime = ctime;
+            }
+
+            public int getNum() {
+                return num;
+            }
+
+            public void setNum(int num) {
+                this.num = num;
+            }
+
+            public String getFoodtupian() {
+                return foodtupian;
+            }
+
+            public void setFoodtupian(String foodtupian) {
+                this.foodtupian = foodtupian;
+            }
+
+            public String getFoot_jianjie() {
+                return foot_jianjie;
+            }
+
+            public void setFoot_jianjie(String foot_jianjie) {
+                this.foot_jianjie = foot_jianjie;
+            }
+
+            public String getRiqi() {
+                return riqi;
+            }
+
+            public void setRiqi(String riqi) {
+                this.riqi = riqi;
+            }
+
+            public String getFenlei() {
+                return fenlei;
+            }
+
+            public void setFenlei(String fenlei) {
+                this.fenlei = fenlei;
+            }
+
+            public String getChushi() {
+                return chushi;
+            }
+
+            public void setChushi(String chushi) {
+                this.chushi = chushi;
+            }
+
+            public String getTui_time() {
+                return tui_time;
+            }
+
+            public void setTui_time(String tui_time) {
+                this.tui_time = tui_time;
+            }
+
+            public String getYuyue_start() {
+                return yuyue_start;
+            }
+
+            public void setYuyue_start(String yuyue_start) {
+                this.yuyue_start = yuyue_start;
+            }
+
+            public String getDingdanid() {
+                return dingdanid;
+            }
+
+            public void setDingdanid(String dingdanid) {
+                this.dingdanid = dingdanid;
+            }
+
+            public String getShiwu_id() {
+                return shiwu_id;
+            }
+
+            public void setShiwu_id(String shiwu_id) {
+                this.shiwu_id = shiwu_id;
+            }
+
+            public String getShijianduan() {
+                return shijianduan;
+            }
+
+            public void setShijianduan(String shijianduan) {
+                this.shijianduan = shijianduan;
             }
         }
     }

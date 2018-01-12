782470b18989fead950ec73903cd4fc3d529a319
ligangqianggit <962027269@qq.com>
2018-01-12 09:29:29 +0800

    修改订餐详情activity

--- app/src/main/java/com/stateunion/eatshop/view/baseactivity/DDXiangQingActivity.java
+++ app/src/main/java/com/stateunion/eatshop/view/baseactivity/DDXiangQingActivity.java
@@ -1,13 +1,18 @@
 package com.stateunion.eatshop.view.baseactivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.stateunion.eatshop.R;
 import com.stateunion.eatshop.adapter.DDxiangqingListAdapter;
@@ -25,10 +30,10 @@ import retrofit2.Call;
  */
 
 public class DDXiangQingActivity extends BaseActivity{
-    String order_sn;
+      String order_sn;
     public static ListView list_ddxiangqiang;
-
     private ImageView iv_ddxiangqiang_back;
+    private Button bt_ddxiangqiang_tuiorpoing;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,18 @@ public class DDXiangQingActivity extends BaseActivity{
         intview();
     }
     public void intview(){
+
+        bt_ddxiangqiang_tuiorpoing= (Button) findViewById(R.id.bt_ddxiangqiang_tuiorpoing);
+        bt_ddxiangqiang_tuiorpoing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialog();
+//                Intent intent=new Intent(DDXiangQingActivity.this,YGPingjiaActivity.class);
+//                intent.putExtra("order_sn",order_sn);
+//                startActivity(intent);
+            }
+        });
+
         list_ddxiangqiang= (ListView) findViewById(R.id.list_ddxiangqiang);
         iv_ddxiangqiang_back= (ImageView) findViewById(R.id.iv_ddxiangqiang_back);
         iv_ddxiangqiang_back.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +77,53 @@ public class DDXiangQingActivity extends BaseActivity{
             list_ddxiangqiang.setAdapter(dDxiangqingListAdapter);
         }
     }
+
+    private void showAlertDialog() {
+        final AlertDialog dialog = new AlertDialog.Builder(this).create();
+        dialog.setView(LayoutInflater.from(this).inflate(R.layout.alert_dialog, null));
+        dialog.show();
+        dialog.getWindow().setContentView(R.layout.alert_dialog);
+        Button btnPositive = (Button) dialog.findViewById(R.id.bt_dialog_tuidan);
+        Button btnNegative = (Button) dialog.findViewById(R.id.bt_dialog_quxiao);
+        final EditText etContent = (EditText) dialog.findViewById(R.id.et_dialog_message);
+        btnPositive.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                String str = etContent.getText().toString();
+                if (isNullEmptyBlank(str)) {
+                    etContent.setError("输入内如不能为空");
+                } else {
+                    dialog.dismiss();
+                    RequestCommand.setTuidan(new TuiDanCallBack(DDXiangQingActivity.this),AppSessionEngine.getLoginInfo().getGonghao().toString(),order_sn,etContent.getText().toString());
+                    Toast.makeText(DDXiangQingActivity.this, etContent.getText().toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        btnNegative.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                dialog.dismiss();
+            }
+        });
+    }
+    private static boolean isNullEmptyBlank(String str) {
+        if (str == null || "".equals(str) || "".equals(str.trim()))
+            return true;
+        return false;
+    }
+
+    public class TuiDanCallBack extends DialogCallback<BaseBean,DDXiangQingActivity>{
+        public TuiDanCallBack(DDXiangQingActivity requestView) {
+            super(requestView);
+        }
+        @Override
+        protected void onResponseSuccess(BaseBean baseBean, Call<BaseBean> call) {
+            super.onResponseSuccess(baseBean, call);
+        }
+    }
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {

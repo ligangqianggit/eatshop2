2315039110094dc451ade958c1f57f016322a263
ligangqianggit <962027269@qq.com>
2018-01-13 17:11:57 +0800

    新增越支付bean

---
+++ app/src/main/java/com/stateunion/eatshop/retrofit/entiity/YuZhiFuBean.java
@@ -0,0 +1,26 @@
+package com.stateunion.eatshop.retrofit.entiity;
+
+import com.stateunion.eatshop.retrofit.bean.BaseBean;
+
+/**
+ * Created by 青春 on 2018/1/13.
+ */
+
+public class YuZhiFuBean extends BaseBean{
+
+    /**
+     * time : 1515831662
+     * code : null
+     * body : 2018011316210281418
+     */
+
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}

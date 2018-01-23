c55d745a2955c70612b1c02501665a49d7d12391
ligangqianggit <962027269@qq.com>
2018-01-23 17:14:25 +0800

    修改配置

--- app/src/main/java/com/stateunion/eatshop/commons/Constants.java
+++ app/src/main/java/com/stateunion/eatshop/commons/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     // 系统版本
     public static int SYSTEM_VERSION;
     public static final long VERSION_UPDATE_CONTROL_TIME = 3 * 24 * 60 * 60 * 1000;
-    public static final String APP_ID = "12121212";
+    public static final String APP_ID = "wx8596138b3771e45";
 
     public static final String API_KEY = "12121212";
 

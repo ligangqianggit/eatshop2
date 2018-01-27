e6d1d87e9a98e6033e6a68bef0f26dd9c8169272
ligangqianggit <962027269@qq.com>
2018-01-27 15:00:07 +0800

    修改Adapter

--- app/src/main/java/com/stateunion/eatshop/adapter/DDxiangqingListAdapter.java
+++ app/src/main/java/com/stateunion/eatshop/adapter/DDxiangqingListAdapter.java
@@ -1,14 +1,17 @@
 package com.stateunion.eatshop.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.stateunion.eatshop.R;
 import com.stateunion.eatshop.retrofit.entiity.OrderBean;
+import com.stateunion.eatshop.view.baseactivity.YGPingjiaActivity;
 
 import java.util.List;
 
@@ -42,25 +45,51 @@ public class DDxiangqingListAdapter extends BaseAdapter{
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int i, View view, ViewGroup viewGroup) {
         Zujian zujian=null;
         if(zujian==null){
             zujian=new Zujian();
-            view=layoutInflater.inflate(R.layout.item_history_two,null);
-            zujian.tv_history_caipin= (TextView) view.findViewById(R.id.tv_history_caipin);
+            view=layoutInflater.inflate(R.layout.item_ddxingqiang,null);
 
-            zujian.tv_history_moneyone= (TextView) view.findViewById(R.id.tv_history_moneyone);
+            zujian.tv_history_caipin= (TextView) view.findViewById(R.id.tv_dingxiang_caipin);
+            zujian.tv_history_moneyone= (TextView) view.findViewById(R.id.tv_dingxiang_moneyone);
+            zujian.bt_dingxiang_pingjia= (Button) view.findViewById(R.id.bt_dingxiang_pingjia);
+            zujian.tv_history_shuliang= (TextView) view.findViewById(R.id.tv_dingxiang_shuliang);
+            if(orderBeanInfoList.get(i).getPing_zhuang().equals("0")){
+                zujian.bt_dingxiang_pingjia.setVisibility(View.GONE);
+            }
+            if(orderBeanInfoList.get(i).getPing_zhuang().equals("1")){
+                //未评
+                zujian.bt_dingxiang_pingjia.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent=new Intent(context, YGPingjiaActivity.class);
 
-            zujian.tv_history_shuliang= (TextView) view.findViewById(R.id.tv_history_shuliang);
+                        intent.putExtra("id",orderBeanInfoList.get(i).getId()+"");//商品id
+                        intent.putExtra("shiwuid",orderBeanInfoList.get(i).getShiwu_id());
+                        intent.putExtra("caiming",orderBeanInfoList.get(i).getFoot_name());
+                        intent.putExtra("chushi",orderBeanInfoList.get(i).getChushi());
+
+                        context.startActivity(intent);
+                    }
+                });
+
+            }
+            if(orderBeanInfoList.get(i).getPing_zhuang().equals("2")){
+                //已评价
+                zujian.bt_dingxiang_pingjia.setText("已评价");
+                zujian.bt_dingxiang_pingjia.setEnabled(false);
+            }
             zujian.tv_history_caipin.setText(orderBeanInfoList.get(i).getFoot_name());
-
-            zujian.tv_history_moneyone.setText(orderBeanInfoList.get(i).getJiage()+"元");
+            zujian.tv_history_moneyone.setText("单价:"+orderBeanInfoList.get(i).getJiage()+"元");
             zujian.tv_history_shuliang.setText("数量:"+orderBeanInfoList.get(i).getGoods_num());
+
 
         }
         return view;
     }
     public final class Zujian{
         public TextView tv_history_caipin,tv_history_moneyone,tv_history_shuliang;
+        public Button bt_dingxiang_pingjia;
     }
 }

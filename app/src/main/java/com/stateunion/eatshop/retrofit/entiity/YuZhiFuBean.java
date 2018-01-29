13d0c3f02e62f2bf1a13dbc4cde93675295ddfde
ligangqianggit <962027269@qq.com>
2018-01-29 15:43:36 +0800

    bena更新

--- app/src/main/java/com/stateunion/eatshop/retrofit/entiity/YuZhiFuBean.java
+++ app/src/main/java/com/stateunion/eatshop/retrofit/entiity/YuZhiFuBean.java
@@ -1,6 +1,5 @@
 package com.stateunion.eatshop.retrofit.entiity;
 
-import com.google.gson.annotations.SerializedName;
 import com.stateunion.eatshop.retrofit.bean.BaseBean;
 
 /**
@@ -10,17 +9,54 @@ import com.stateunion.eatshop.retrofit.bean.BaseBean;
 public class YuZhiFuBean extends BaseBean{
 
     /**
-     * time : 1515831662
+     * time : 1517210433
      * code : null
-     * body : 2018011316210281418
+     * body : {"prepayid":"","noncestr":"","sign":""}
      */
-    private String body;
-    public String getBody() {
+
+    private BodyBean body;
+
+    public BodyBean getBody() {
         return body;
     }
 
-    public void setBody(String body) {
+    public void setBody(BodyBean body) {
         this.body = body;
     }
 
+    public static class BodyBean {
+        /**
+         * prepayid :
+         * noncestr :
+         * sign :
+         */
+
+        private String prepayid;
+        private String noncestr;
+        private String sign;
+
+        public String getPrepayid() {
+            return prepayid;
+        }
+
+        public void setPrepayid(String prepayid) {
+            this.prepayid = prepayid;
+        }
+
+        public String getNoncestr() {
+            return noncestr;
+        }
+
+        public void setNoncestr(String noncestr) {
+            this.noncestr = noncestr;
+        }
+
+        public String getSign() {
+            return sign;
+        }
+
+        public void setSign(String sign) {
+            this.sign = sign;
+        }
+    }
 }

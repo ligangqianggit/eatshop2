6a17a604fa777f6f27657f3b636680d65c546d7c
ligangqianggit <962027269@qq.com>
2018-03-02 11:18:36 +0800

    推送

--- app/src/main/java/com/stateunion/eatshop/service/DemoIntentService.java
+++ app/src/main/java/com/stateunion/eatshop/service/DemoIntentService.java
@@ -1,12 +1,17 @@
 package com.stateunion.eatshop.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.igexin.sdk.GTIntentService;
 import com.igexin.sdk.PushManager;
 import com.igexin.sdk.message.GTCmdMessage;
 import com.igexin.sdk.message.GTTransmitMessage;
+import com.stateunion.eatshop.R;
 
 /**
  * Created by 青春 on 2017/12/28.
@@ -45,6 +50,7 @@ public class DemoIntentService extends GTIntentService{
         }
         String data = new String(payload);
         Log.d("eatshop", "receiver payload = " + data);
+        showNotifictionIcon(context,data);
     }
 
     @Override
@@ -59,6 +65,22 @@ public class DemoIntentService extends GTIntentService{
 
     @Override
     public void onReceiveCommandResult(Context context, GTCmdMessage cmdMessage) {
+    }
+
+    public static void showNotifictionIcon(Context context,String mes) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+//        Intent intent = new Intent(context, PlayActivity.class);//将要跳转的界面
+        builder.setAutoCancel(true);//点击后消失
+        builder.setSmallIcon(R.drawable.push);//设置通知栏消息标题的头像
+        builder.setDefaults(NotificationCompat.DEFAULT_SOUND);//设置通知铃声
+//        builder.setTicker("你好");
+        builder.setContentText(mes);//通知内容
+        builder.setContentTitle("中融食堂");
+        //利用PendingIntent来包装我们的intent对象,使其延迟跳转
+//        PendingIntent intentPend = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+//        builder.setContentIntent(intentPend);
+        NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
     }
 
 }
